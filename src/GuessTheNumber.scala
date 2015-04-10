56748a1981a148fe302cf172443cb838f6c44d36
jeffolsen <olsenjeffn@gmail.com>
2015-04-10 07:44:52 -0600

    wip number bounds check

--- src/GuessTheNumber.scala
+++ src/GuessTheNumber.scala
@@ -3,7 +3,7 @@
  */
 
 import scala.io.StdIn._
-import scala.util.control.BreakControl
+import scala.util._
 
 object GuessTheNumber {
   def main(args: Array[String]): Unit = {
@@ -12,14 +12,22 @@ object GuessTheNumber {
   }
 
   def PickANumberDialog(message: String): Long = {
-    val number = readLine(message + "\n")
-    return if (number.matches("^-?\\d+(\\.\\d)?$")) number.toLong else PickANumberDialog("That is not a number.\n " + message)
+    val number = List(readLine(message + "\n"))
+
+    return number.find(_ => Long {
+      if (_.matches("^-?\\d+(\\.\\d)?$")) {
+        return if (Try(_.toLong).isSuccess) _ else PickANumberDialog("Whoa whoa, I can't count that far from zero.\n" + message)
+      } else {
+        return PickANumberDialog("That's not a number. " + message)
+      }
+    })
   }
 
   def PickARangeDialog(message: String): List[Long] = {
     println(message)
     val low = PickANumberDialog("Pick a low whole number.")
     val high = PickANumberDialog("Pick a high whole number.")
+
     return if (low < high) List(low, high) else PickARangeDialog("Your high number is not greater than your low number.\n" + message)
   }
 }
