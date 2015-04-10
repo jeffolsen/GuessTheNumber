6d85d3b97908bcec7b59957af4550cf5469943a7
jeffolsen <olsenjeffn@gmail.com>
2015-04-10 06:53:59 -0600

    Clean up PickANumber and PickARange

--- src/GuessTheNumber.scala
+++ src/GuessTheNumber.scala
@@ -7,37 +7,19 @@ import scala.util.control.BreakControl
 
 object GuessTheNumber {
   def main(args: Array[String]): Unit = {
-
-    val range = PickARange()
+    val range = PickARange("Pick a number range.")
     println("you picked " + range(0) + " and " + range(1))
   }
 
   def PickANumber(message: String): Int = {
     val number = readLine(message + "\n")
-
-    if (number.matches("^-?\\d+(\\.\\d)?$"))
-    {
-      return number.toInt
-    }
-    else
-    {
-      println("that is not a number")
-      return PickANumber(message)
-    }
+    return if (number.matches("^-?\\d+(\\.\\d)?$")) number.toInt else PickANumber("That is not a number.\n " + message)
   }
 
-  def PickARange(): List[Int] = {
-    val low = PickANumber("Pick a low whole number")
-    val high = PickANumber("Pick a high whole number")
-
-    if (low < high)
-    {
-      return List(low, high)
-    }
-    else
-    {
-      println("Your high number is not greater than your low number")
-      return PickARange()
-    }
+  def PickARange(message: String): List[Int] = {
+    println(message)
+    val low = PickANumber("Pick a low whole number.")
+    val high = PickANumber("Pick a high whole number.")
+    return if (low < high) List(low, high) else PickARange("Your high number is not greater than your low number.\n" + message)
   }
 }
